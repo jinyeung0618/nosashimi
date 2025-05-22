5a867d9e653262e22bd448ea0172699d70e227d1
kcarl <jinyeung0618@gmail.com>
2025-05-22 20:45:47 +0900

    Initial Commit

---
+++ nosashimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nosashimi
+//
+//  Created by 최진영 on 5/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ nosashimiUITests/nosashimiUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  nosashimiUITestsLaunchTests.swift
+//  nosashimiUITests
+//
+//  Created by 최진영 on 5/22/25.
+//
+
+import XCTest
+
+final class nosashimiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
